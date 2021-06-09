356715e41dac3663b28fcc4def892e16367b3dcd
Ahmed Elbassal <ahmed.bassal.italy@gmail.com>
2021-06-10 01:19:01 +0200

    upload

---
+++ DevelopigCommunityService/Controllers/new class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopigCommunityService.Controllers
+{
+    public class new_class
+    {
+    }
+}
